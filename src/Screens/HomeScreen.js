45c9a612f4c9864862b231790470a91726d8576a
machakala <yusuffhamzat.yh@gmail.com>
2021-10-05 19:58:53 +0800

    homescreen changes

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,12 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function HomeScreen(props) {
-    const handleContact = (e) => {
-        props.history.push('/contact')
-    }
-    const handleEvent = (e) => {
-        props.history.push('/event')
-    }
+
     return (
         <div>
             <div className='home'>
@@ -15,16 +11,10 @@ function HomeScreen(props) {
                     <div className='homewords2'>
                         I warmly welcome you to SGF Private Nursery/Primary School, the school of your DREAM. SGF identifies with the individual needs of every Child as a talented gift from God. At SGF every Child is trained to recognize and achieve his/her fullest potential. To reach their destination of excellence, every Child needs a good foundation. By the grace of God, SGF emphasizes and balances the Moral, Social, Emotional, Physical and intellectual development of each Child.
                         <div className='homebutton'>
-                            <button
-                                className='homebutton1'
-                                onClick={handleContact}>
-                                Contact US
-                            </button>
-                            <button
-                                className='homebutton2'
-                                onClick={handleEvent}>
-                                Upcoming Event
-                            </button>
+                            <button className='homebutton1'><Link to="/contact"> Contact Us</Link></button>
+
+                            <button className='homebutton2'><Link to="/event"> Upcoming events</Link></button>
+
 
                         </div>
                     </div>
